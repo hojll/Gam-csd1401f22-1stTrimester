e74dd934e025f259c2d0f903aa0431a1d32274a4
joellohki <110614623+joellohki@users.noreply.github.com>
2022-10-28 15:01:13 +0800

    Update ui.h
    Fixed header file

--- VS_test/Src/ui.h
+++ VS_test/Src/ui.h
@@ -5,7 +5,7 @@ void set_timer(float time_in_seconds);
 
 void update_timer(void);
 
-void bar_system(void);
+void bar_system(float x, float y, float width, float height, int value, int total_value);
 
 void update_bullet_bar(int current_bullet_count, int total_bullet_count);
 #endif // !UI_H
